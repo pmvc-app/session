b1fde84c229505de6cba8b0df07731812a171175
Hill <hill@kimo.com>
2017-02-16 16:21:14 +0000

    revamp

--- index_api.php
+++ index_api.php
@@ -7,17 +7,21 @@ $yo=\PMVC\plug('yo');
 
 $yo->get('/session/{id}', function($m, $f){
    $session = \PMVC\plug('guid')->getDb('session');
+   $id = $f['id'];
    $go = $m['dump'];
-   $go->set('id',$f['id']);
-   $go->set('session',(string)$session[$f['id']]);
+   $go->set('id',$id);
+   $go->set('session',$session[$id]);
    return $go;
 });
 
 $yo->post('/session/{id}', function($m, $f){
    $session = \PMVC\plug('guid')->getDb('session');
-   $session[$f['id']] = $f['data'];
-   $go->set('id',$f['id']);
-   $go->set('session',(string)$session[$f['id']]);
+   $id = $f['id'];
+   $session[$id] = $f['data'];
+   $go = $m['dump'];
+   $go->set('id',$id);
+   $go->set('session',$session[$id]);
+   return $go;
 });
 
 $yo->delete('/session/{id}', function($m, $f){
