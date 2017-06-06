97c141731e935683d970e2f3255c045c11739e45
Hill <hill@kimo.com>
2017-06-06 15:02:10 +0000

    Add auto update expire

--- index_api.php
+++ index_api.php
@@ -10,8 +10,14 @@ $yo->get('/session/{id}', function($m, $f){
    $id = $f['id'];
    $go = $m['dump'];
    $go->set('id',$id);
-   $go->set('session',$session[$id]);
-   $go->set('ttl', $session->ttl($id));
+   if (isset($session[$id])) {
+       $lifeTime = \PMVC\plug('session')->getLifeTime();
+       if ($lifeTime) {
+            $result = $session->setExpire($id, $lifeTime);
+       }
+       $go->set('session',$session[$id]);
+       $go->set('expire', date('Y/m/d H:i:s', time()+$session->ttl($id)));
+   }
    return $go;
 });
 
